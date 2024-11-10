8cb0ade55a15a7c3a57d1fa33f431b261180a632
kawsar-uddin <kawsaruddin450@gmail.com>
2024-11-10 12:54:51 +0600

    keep track of the comments and show details

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,116 @@
 import { useState } from 'react';
-import './App.css'
 
-function App() {
-  const [modalPosition, setModalPosition] = useState({ x: 0, y: 0 });
-  const [showModal, setShowModal] = useState(false);
+const App = () => {
+    const [modalPosition, setModalPosition] = useState({ x: 0, y: 0 });
+    const [showModal, setShowModal] = useState(false);
+    const [comments, setComments] = useState([]);
+    const [newComment, setNewComment] = useState('');
+    const [selectedComment, setSelectedComment] = useState(null);
 
-  const handlePageClick = (e) => {
-    // Get click coordinates relative to the viewport
-    const { clientX, clientY } = e;
-    setModalPosition({ x: clientX, y: clientY });
-    setShowModal(true); // Show the modal at click position
-  };
+    const handlePageClick = (e) => {
+        const { pageX, pageY } = e;
+        setModalPosition({ x: pageX, y: pageY });
+        setShowModal(true);
+    };
 
-  const closeModal = (event) => {
-    const form = event.target;
-    const comment = form.comment.value;
-    console.log(comment);
-    setShowModal(false);
-  };
+    const handleCommentSubmit = () => {
+        setComments([...comments, { id: Date.now(), text: newComment, position: modalPosition }]);
+        setNewComment('');
+        setShowModal(false);
+    };
 
-  return (
-    <div onClick={handlePageClick} style={{ height: '100vh', cursor: 'pointer' }}>
-      <h2>Page Title</h2>
+    const handleMarkerClick = (comment) => {
+        setSelectedComment(comment);
+    };
 
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo nostrum perferendis voluptas praesentium voluptate fuga cum quaerat dicta numquam perspiciatis odit, assumenda architecto exercitationem dignissimos cumque magni quo corporis. Et rerum nostrum voluptate odio nobis cum officia repudiandae reiciendis in assumenda ad aspernatur quo, sequi sint numquam. Doloremque harum commodi deleniti iure qui architecto consequuntur ipsa aliquam esse, voluptas possimus quasi ipsum voluptatum rem ut nobis odio cum beatae quia, veritatis provident corporis. Iusto non neque aperiam cupiditate dolore tempora excepturi reiciendis, veniam corrupti deserunt, blanditiis in aliquam est quis corporis maxime temporibus similique libero? Repellendus error odit veniam quisquam!</p>
-      <br /> <br />
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod natus aliquam consectetur excepturi sed eius earum, accusamus quas, tempora minus, laudantium doloribus architecto harum cumque magni! Iusto reiciendis quas dolorem consequuntur accusantium. Nobis veniam cupiditate sapiente blanditiis deserunt voluptatum unde quam itaque molestiae est impedit provident at sunt in nostrum quibusdam illo earum veritatis inventore, quasi accusantium alias esse! Quaerat impedit deleniti, cum tempora nesciunt ea id ipsa harum necessitatibus blanditiis omnis, animi voluptatum perspiciatis tenetur. Amet voluptatum facere laboriosam laudantium quasi ex ipsa? Nesciunt tenetur sequi eveniet explicabo officiis ullam itaque id laboriosam, quam tempore dignissimos vel, exercitationem corrupti.</p>
-      <br /> <br />
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores dignissimos voluptate repellendus aliquam quis quisquam omnis quas adipisci consequatur iure voluptatem dolore et, similique ad veniam id eos, dolorum beatae consequuntur iste ratione quam non quo? Id qui dolores officiis quam excepturi corrupti voluptate iste eos error ea, possimus voluptatem magnam quos? Rem, dignissimos quo! Voluptates nostrum quis atque amet expedita ex. Sapiente perspiciatis facilis obcaecati ut nihil corporis nostrum. Consequatur, accusamus dicta ipsam eligendi ex animi eaque quia aliquam repellendus neque saepe explicabo voluptatibus minus placeat est odit rerum corrupti omnis tempora. Provident eligendi tempora, molestiae libero ipsa cum.</p>
+    const closeCommentPopup = () => {
+        setSelectedComment(null);
+    };
 
+    return (
+        <div onClick={handlePageClick} style={{ height: '100vh', cursor: 'pointer' }}>
+            <h1>Click anywhere to leave a comment!</h1>
+            <br /> <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, quam error? Hic vitae rem iusto voluptatem consequuntur quasi et animi, laudantium ab libero facere veritatis pariatur blanditiis totam voluptates quod nam vero! Consectetur commodi doloremque quo? Quae illo a hic! Harum incidunt molestias repellendus esse libero cumque architecto deleniti? In impedit laudantium dolore error ipsum soluta eligendi pariatur alias nulla reiciendis enim, ex dignissimos. Officia eos voluptates expedita? Aut minima quis officiis. Molestiae itaque voluptas at dignissimos. Molestias molestiae tenetur veritatis, harum tempore voluptate quasi qui, architecto, sit optio dolor inventore? Distinctio tempora hic veritatis consectetur id consequuntur adipisci doloremque.</p>
+            <br /><br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor maiores necessitatibus doloremque adipisci. Accusantium perspiciatis quisquam repudiandae dolorem ducimus libero! Voluptatum tempore repellendus laboriosam enim voluptas optio vitae provident eos iste? Voluptate earum enim sed culpa ab veritatis ea omnis. Perferendis autem minus dolor quas pariatur alias, explicabo ea, nostrum voluptatem culpa tenetur temporibus. Quod, nulla accusantium quidem, molestias maiores perspiciatis dolor iure voluptates deserunt quo id, nesciunt reprehenderit ratione esse et! Totam voluptatem vel nesciunt maiores. Magnam impedit minima dolorum omnis iusto quam minus! Assumenda perspiciatis dolore deserunt nesciunt repellendus natus nihil itaque veniam modi, ullam provident distinctio ipsum.</p>
+            <br /><br />
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur distinctio, animi quisquam labore dolor vero odio eos vel quia quasi tenetur impedit? Maiores impedit numquam aperiam dicta magni delectus ea consequuntur incidunt sint, error ratione natus repudiandae eaque quam est inventore debitis? Amet fugit nesciunt omnis aperiam nihil, necessitatibus itaque quisquam quas nisi est quod aliquam similique delectus rem voluptatem nemo, eius odit, aut soluta ut id. Accusantium officia, corporis officiis ducimus exercitationem ipsa! Quo nobis alias quae dolores hic officia eum ut consectetur. Natus exercitationem ducimus quae sunt consequuntur at, blanditiis iste et omnis amet sint in porro quam?</p>
 
-      {/* Modal */}
-      {showModal && (
-        <div
-          style={{
-            position: 'absolute',
-            top: modalPosition.y,
-            left: modalPosition.x,
-            transform: 'translate(-50%, -50%)', // Center the modal at the click position
-            padding: '20px',
-            backgroundColor: 'white',
-            boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
-            zIndex: 1000
-          }}
-          onClick={(e) => e.stopPropagation()} // Prevent modal close on inner click
-        >
-          <form onSubmit={closeModal}>
-            <label htmlFor="comment">Put your feedback here: </label>
-            <textarea name="comment" id="comment" rows={5}></textarea>
-            <input type='submit' value={"Post"} />
-          </form>
+            {/* Markers for each comment */}
+            {comments.map((comment) => (
+                <div
+                    key={comment.id}
+                    style={{
+                        position: 'absolute',
+                        top: comment.position.y,
+                        left: comment.position.x,
+                        width: '20px',
+                        height: '20px',
+                        backgroundColor: 'red',
+                        borderRadius: '50%',
+                        cursor: 'pointer',
+                        transform: 'translate(-50%, -50%)',
+                    }}
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        handleMarkerClick(comment);
+                    }}
+                />
+            ))}
+
+            {/* Modal for adding a new comment */}
+            {showModal && (
+                <div
+                    style={{
+                        position: 'absolute',
+                        top: modalPosition.y,
+                        left: modalPosition.x,
+                        transform: 'translate(-50%, -50%)',
+                        padding: '20px',
+                        backgroundColor: 'white',
+                        boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
+                        zIndex: 1000
+                    }}
+                    onClick={(e) => e.stopPropagation()}
+                >
+                    <form onSubmit={handleCommentSubmit}>
+                        <label htmlFor="comment">Put your feedback here: </label>
+                        <textarea name="comment" id="comment" rows={5} value={newComment}
+                            onChange={(e) => setNewComment(e.target.value)}
+                            placeholder="Add your comment"></textarea>
+                        <input type='submit' value={"Post"} />
+                    </form>
+                    {/* <textarea
+            value={newComment}
+            onChange={(e) => setNewComment(e.target.value)}
+            placeholder="Add your comment"
+          />
+          <button onClick={handleCommentSubmit}>Submit</button> */}
+                </div>
+            )}
+
+            {/* Popup to show selected comment */}
+            {selectedComment && (
+                <div
+                    style={{
+                        position: 'absolute',
+                        top: selectedComment.position.y,
+                        left: selectedComment.position.x,
+                        transform: 'translate(-50%, -50%)',
+                        padding: '20px',
+                        backgroundColor: 'white',
+                        color: 'black',
+                        boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
+                        zIndex: 1000
+                    }}
+                >
+                    <p>{selectedComment.text}</p>
+                    <button onClick={closeCommentPopup}>Close</button>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  )
-}
+    );
+};
 
-export default App
+export default App;
